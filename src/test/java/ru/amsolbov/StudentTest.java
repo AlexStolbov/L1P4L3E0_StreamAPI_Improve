c7ce7bc5903c0f205698ad40335695b94aedd21d
Alex <stolbov_alexey@mail.ru>
2019-01-10 17:24:20 +0300

    1. Collection API Улучшения [#70623] (1)

--- src/test/java/ru/amsolbov/StudentTest.java
+++ src/test/java/ru/amsolbov/StudentTest.java
@@ -2,7 +2,6 @@ package ru.amsolbov;
 
 import org.junit.Test;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import static org.hamcrest.Matchers.is;
@@ -11,15 +10,13 @@ import static org.junit.Assert.*;
 public class StudentTest {
     @Test
     public void whenCallLevelOffThenGetResult() {
-        List<Student> students = new ArrayList<>();
-        students.add(new Student("Alex", 15));
-        students.add(new Student("Dmitry", 10));
-        students.add(null);
-        students.add(new Student("Piter", 12));
-        students.add(null);
-        List<Student> test = new ArrayList<>();
-        test.add(students.get(0));
-        test.add(students.get(3));
+        List<Student> students = List.of(
+                new Student("Alex", 15),
+                new Student("Dmitry", 10),
+                new Student("Peter", 12));
+        List<Student> test = List.of(
+                students.get(0),
+                students.get(2));
         assertThat(Student.levelOf(students, 11), is(test));
     }
 
